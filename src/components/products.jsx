131e941876aa3007fe6963d80c6f9640b0ec860a
Jason Valadez <valadezj045@gmail.com>
2021-04-23 15:23:27 -0800

    usereducer

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const currencyOptions = {
   minimumFractionDigits: 2,
@@ -23,34 +23,54 @@ const products = [
   }
 ];
 
-function getTotal(total) {
-  return total.toLocaleString(undefined, currencyOptions)
+function getTotal(cart) {
+  const total = cart.reduce((totalCost, item) => totalCost + item.price, 0);
+  return total.toLocaleString(undefined, currencyOptions);
+}
+
+function cartReducer(state, action) {
+  switch(action.type) {
+    case 'add':
+      return [...state, action.product];
+    case 'remove':
+      const productIndex = state.findIndex(item => item.name === action.product.name);
+      if(productIndex < 0) {
+        return state;
+      }
+      const update = [...state];
+      update.splice(productIndex, 1)
+      return update
+    default:
+      return state;
+  }
 }
 
 export default function Product() {
-  const [cart, setCart] = useState([]);
-  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useReducer(cartReducer, []);
 
-  function add() {
-    setCart(['ice cream']);
-    setTotal(5);
+  function add(product) {
+    setCart({ product, type: 'add' });
   }
 
+  function remove(product) {
+    setCart({ product, type: 'remove' });
+  }
 
   return(
     <div className="wrapper">
       <div>
         Shopping Cart: {cart.length} total items.
       </div>
-      <div>Total: {getTotal(total)}</div>
-        <div>
+      <div>Total: {getTotal(cart)}</div>
+
+      <div>
         {products.map(product => (
           <div key={product.name}>
             <div className="product">
               <span role="img" aria-label={product.name}>{product.emoji}</span>
             </div>
-            <button>Add</button>
-            <button>Remove</button>
+            <button onClick={() => add(product)}>Add</button>
+            <button onClick={() => remove(product)}>Remove</button>
           </div>
         ))}
       </div>
